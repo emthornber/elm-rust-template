4feb56e2998d7ec6e8beb28a259ae31e01df9825
E M Thornber <enchanted.systems@btinternet.com>
2025-03-15 11:34:15 +0000

    Bump rust dependency versions. Remove use of wee_alloc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
 use std::convert::TryInto;
+use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// // allocator.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn fibonacci(nth: i32) -> i32 {
     if nth < 0 {
-        -1i32.pow((-nth+1).try_into().unwrap()) * fibonacci(-nth)
+        -1i32.pow((-nth + 1).try_into().unwrap()) * fibonacci(-nth)
     } else if nth == 0 {
         1
     } else if nth == 1 {
