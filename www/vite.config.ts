3dc4fd14797eb8301524ffde54ab6d6facbc3e83
E M Thornber <enchanted.systems@btinternet.com>
2025-03-17 10:24:08 +0000

    Absorbed configs from rust-wasm-hello-world and vite-elm-spa.

---
+++ www/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+import wasm from "vite-plugin-wasm";
+// not needed but important for supporting older browsers
+import topLevelAwait from "vite-plugin-top-level-await";
+import elmPlugin from 'vite-plugin-elm'
+
+export default defineConfig({
+    root: '.',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(import.meta.url, 'index.html'),
+                bootstrap: resolve(import.meta.url, 'bootstrap.js')
+            }
+        }
+    },
+    server: {
+        open: 'index.html'
+    },
+    plugins: [
+        wasm(),
+        topLevelAwait(),
+        elmPlugin(),
+    ]
+})
